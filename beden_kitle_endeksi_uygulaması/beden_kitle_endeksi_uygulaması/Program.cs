0ff666a8037e1ef9d65b2b8ad05f748b263918cc
Beyza Nur Koç <beyzannkoc@gmail.com>
2023-08-13 14:54:53 +0300

    beden_kitle_endeksi_uygulaması
    c# ile yazılmıştır. Boy ve kilo değerlerinizi girdikten sonra sistem çalışıyor ve "zayifisniz/normalsiniz/obezsiniz" uyarılarını veriyor

---
+++ beden_kitle_endeksi_uygulaması/beden_kitle_endeksi_uygulaması/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beden_kitle_endeksi_uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // beden kitle endeksi = kilo/ boy*boy 
+            // x <= 18 ise zayıf 
+            // 18 < x < 25 ise normal 
+            // 25 < x ise obez 
+
+            Console.WriteLine("lutfen kilonuzu giriniz ");
+            int kilo = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("lutfen boyunuzu giriniz");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (boy * boy) ;
+
+            if (bke < 18)
+            {
+                Console.WriteLine("Zayifsiniz");
+                Console.ReadLine();
+            }
+            else if ( bke > 18  && bke < 25)
+            {
+                Console.WriteLine("Normalsiniz");
+                Console.ReadLine();
+            }
+            else if (25 < bke)
+            {
+                Console.WriteLine("Obezsiniz");
+                Console.ReadLine(); 
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
